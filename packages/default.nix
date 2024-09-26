1a9d91a9bbee906e6c2020676521039dd957577f
Chris Montgomery <chrismont@temple.edu>
2024-09-26 14:50:51 -0400

    feat: provide patched ddev package expression for intellij plugin compat

--- packages/default.nix
+++ packages/default.nix
@@ -5,6 +5,9 @@
     { pkgs, ... }:
     {
       packages = {
+        # TODO: Remove when available upstream: https://github.com/NixOS/nixpkgs/pull/344503
+        ddev = pkgs.callPackage ./ddev.nix { };
+
         php-stubs-generator = pkgs.callPackage ./php-stubs-generator/package.nix { };
       };
     };
