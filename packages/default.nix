de7a580480ed3a0f6a420708587e0f10ecb9d5bd
Chris Montgomery <chrismont@temple.edu>
2025-07-16 15:46:12 -0400

    refactor(php-lint): provide real package expressions

--- packages/default.nix
+++ packages/default.nix
@@ -1,12 +1,21 @@
+{ lib, ... }:
 {
-  imports = [ ./php-lint.nix ];
-
   perSystem =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
+      apps = {
+        php-lint-project = {
+          type = "app";
+          program = lib.getExe config.packages.php-lint-project;
+        };
+      };
       packages = {
         # TODO: Remove when available upstream: https://github.com/NixOS/nixpkgs/pull/344503
         ddev = pkgs.callPackage ./ddev/package.nix { };
+        php-lint = pkgs.callPackage ./php-lint/package.nix { };
+        php-lint-project = pkgs.callPackage ./php-lint/php-lint-project.nix {
+          inherit (config.packages) php-lint;
+        };
         php-stubs-generator = pkgs.callPackage ./php-stubs-generator/package.nix { };
         shib-keygen = pkgs.callPackage ./shib-keygen/package.nix { };
       };
