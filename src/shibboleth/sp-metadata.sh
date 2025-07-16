b99fb31171336b75d0c6701d0b426fbb5a44f1ba
Chris Montgomery <chrismont@temple.edu>
2025-07-16 14:19:56 -0400

    chore: remove obsolete shibboleth script

--- src/shibboleth/sp-metadata.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p gum mustache-go
-
-FQDN=$(gum input --placeholder 'Site FQDN?')
-
-echo "siteFqdn: $FQDN" \
-| mustache ./sp-metadata.xml.mustache \
-> "sp-metadata.$FQDN.xml.txt"
