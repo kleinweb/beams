6e4a820503cdfb5c1c9a35e10387db84deb5d64e
Chris Montgomery <chrismont@temple.edu>
2025-07-16 15:21:15 -0400

    refactor(ddev): move package to standard location

--- packages/default.nix
+++ packages/default.nix
@@ -6,10 +6,8 @@
     {
       packages = {
         # TODO: Remove when available upstream: https://github.com/NixOS/nixpkgs/pull/344503
-        ddev = pkgs.callPackage ./ddev.nix { };
-
+        ddev = pkgs.callPackage ./ddev/package.nix { };
         php-stubs-generator = pkgs.callPackage ./php-stubs-generator/package.nix { };
-
         shib-keygen = pkgs.callPackage ./shib-keygen/package.nix { };
       };
     };
