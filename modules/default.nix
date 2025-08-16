3cf4e46026787172e52b66a7a981d091f3d192e6
Chris Montgomery <chrismont@temple.edu>
2025-08-16 17:14:41 -0400

    feat: provide default profiles as modules

--- modules/default.nix
+++ modules/default.nix
@@ -5,11 +5,31 @@
     };
 
     nixos = {
+      default = {
+        imports = [
+          ./ddev/module.nix
+          ./kleinweb/dev/module.nix
+          ./kleinweb/dev/hosts-entries/module.nix
+        ];
+
+        programs.ddev.enable = true;
+        kleinweb.dev.enable = true;
+        kleinweb.dev.hostsEntries.enable = true;
+      };
       ddev = ./ddev/module.nix;
       kleinweb-dev__hosts-entries = ./kleinweb/dev/hosts-entries/module.nix;
     };
 
     home = {
+      default = {
+        imports = [
+          ./kleinweb/dev/module.nix
+          ./kleinweb/dev/ssh/aliases.nix
+          ./kleinweb/dev/tools/module.nix
+        ];
+
+        kleinweb.dev.tools.enable = true;
+      };
       kleinweb-dev__ssh-aliases = ./kleinweb/dev/ssh/aliases.nix;
       kleinweb-dev__tools = ./kleinweb/dev/tools/module.nix;
     };
